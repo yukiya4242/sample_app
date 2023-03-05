c2ff0972956f4442386d20b948689dcd94a50c24
yukiya4242 <126574597+yukiya4242@users.noreply.github.com>
2023-03-05 08:21:58 +0000

    none

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   post 'lists'=>'lists#create'
   get 'lists'=>'lists#index'
   get 'lists/:id'=>'lists#show',as:'list'
-  get 'lists/edit'
+  get 'lists/:id/edit'=>'lists#edit',as:'edit_list'
   get '/top'=>'homes#top'
+  resources :lists
+
+  patch 'lists/:id'=>'lists#update',as:'update_list'
+  delete '/lists/:id'=>'lists#destroy',as:'destroy_list'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
