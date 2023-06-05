ddcd7b94cfcfaf33e12d69f647ce45218abd1d66
yukiya4242 <126574597+yukiya4242@users.noreply.github.com>
2023-06-05 11:58:07 +0000

    Add login and edit header

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
+      login user
+      redirect_to user
     else
       flash.now[:danger] = "ユーザーのメールアドレスが見つかりませんでした"
       render 'new'
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,23 @@
 module SessionsHelper
+
+  def login(user)
+    session[:user_id] = user.id
+  end
+
+  def current_user
+    #current_userがnilかどうかはsession_idがあるか、つまり一度でもログインしているか
+    if session[:user_id]
+      #DBへの問い合わせをできる限り小さくしたい↓
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+  end
+
+  #ユーザーがログインしてるのか確認。
+  def logged_in?
+    #[!]はnotオペレーターと呼ばれる
+    #current_userがnilではない(つまり存在するなら) => ture
+    #current_userがnil(つまり存在しないなら) => false
+    !current_user.nil?
+  end
+
 end
