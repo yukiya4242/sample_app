5e105f4434aa944fb15ae13a5255d052062ccc39
yukiya4242 <126574597+yukiya4242@users.noreply.github.com>
2023-06-11 01:09:46 +0000

    Add Admin User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only:[:edit, :update, :idnex]
+  before_action :logged_in_user, only:[:edit, :update, :idnex, :destroy]
   before_action :correct_user,   only:[:edit, :update]
+  before_action :admin_user,     only: :destroy
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
 
   def show
     @user = User.find(params[:id])
@@ -38,6 +43,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User.deleted"
+    redirect_to users_url
+  end
+
 
 
   private
@@ -59,4 +70,10 @@ class UsersController < ApplicationController
         redirect_to login_url, status: :see_other #see_otherはPOSTやDELETEアクションに使われる
       end
     end
+
+    #管理者かどうか確認
+    def admin_user
+      redirect_to(root_url, status: :see_other) unless current_user.admin?
+    end
+
 end
